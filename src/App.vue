fac3e03ade75397b833e5a6f2dfdf21984c99299
TARKAN <tarkancengiz108@gmail.com>
2025-08-30 02:07:40 +0300

    feat: lucide and homepage architecture added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,63 @@
-<script setup lang="ts">
+<template>
+  <div class="w-screen h-screen flex flex-col">
+    <Navbar />
+
+    <div class="flex flex-col">
+      <div class="flex flex-row items-center justify-between px-2 py-4 border-b-2 border-gray-200">
+        <span class="font-bold text-md">134 Appoinments found.</span>
+        <button class="text-white bg-pink-600 p-3 px-4 rounded-lg">Create Appointment</button>
+      </div>
+
+      <div class="flex flex-col">
+
+
+        <div class="flex flex-col gap-4 p-2">
+
+          <div v-for="i in 10" class="flex flex-row items-center w-full bg-blue-300  rounded-lg p-2">
+
+            <div class="flex flex-col w-1/4 items-center justify-start">
+              <div v-for="i in 3" class="flex flex-row gap-4 text-sm items-center justify-center">
+                <div><Search /></div>
+                <div><span>Emma Smith</span></div>
+              </div>
+            </div>
+
+            <div class="flex flex-row w-1/4">
+              <Search /> Kemalöz Mah. Uşak
+            </div>
+
+            <div class="flex flex-col w-1/4">
+
+              <div class=" w-2/3 p-6 bg-pink-600 rounded-2xl"></div>
+
+            </div>
+
+
+            <div class="flex flex-col w-1/4">
+
+              <UserPop />
+            </div>
+
+          </div>
+
+        </div>
+
+
+
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import { Search } from 'lucide-vue-next'
+
+import Navbar from '@/components/Navbar.vue'
+import UserPop from '@/components/UserPop.vue'
 
 </script>
-
-<template>
-  <div class="w-screen h-screen flex flex-col bg-blue-50">
-    <span>TARKAN CENGIZ</span>
-  </div>
-
-</template>
 
 <style scoped>
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="flex flex-row w-full px-2 py-4 border-b-2 border-gray-400">
+
+    <UserPop />
+
+    <div class="flex flex-row gap-4 w-full px-10">
+      <select class="border-gray-200 border-1 rounded-lg px-4 py-2 text-sm w-64 focus:outline-0">
+        <option>All Statuses</option>
+        <option>Cancelled</option>
+        <option>Upcoming</option>
+        <option>Completed</option>
+      </select>
+
+      <input
+        type="datetime-local"
+        class="border rounded-lg border-gray-200 px-3 py-2 text-sm pr-10 focus:outline-0"
+      />
+
+      <input
+        type="datetime-local"
+        class="border rounded-lg border-gray-200 px-3 py-2 text-sm pr-10 focus:outline-0"
+      />
+
+    </div>
+
+    <div class="flex flex-row w-1/6">
+      <input
+        type="text"
+        class="border-2 rounded-lg rounded-r-none border-r-0 border-gray-200 px-3 py-2 text-sm pr-10 focus:outline-0 placeholder:text-center w-96"
+        placeholder="Search..."
+      />
+      <button class="rounded-lg rounded-l-none border-gray-200 border-2 w-16 flex items-center justify-center focus:border-pink-600">
+        <Search class="w-5 h-5"/>
+
+      </button>
+    </div>
+
+  </div>
+</template>
+<script setup lang="ts">
+import { Search } from 'lucide-vue-next'
+import UserPop from './UserPop.vue'
+
+</script>
