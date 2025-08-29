2975f05b837626e65d2c98f5a677fcb05634ce33
TARKAN <tarkancengiz108@gmail.com>
2025-08-30 01:16:55 +0300

    feat: tailwind added

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="w-screen h-screen flex flex-col bg-blue-50">
+    <span>TARKAN CENGIZ</span>
+  </div>
+
+</template>
+
+<style scoped>
+</style>
